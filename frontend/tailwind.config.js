7ffaa5f5745755e1af1c878ec95c41cf9f58bb18
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-09-13 17:08:01 +0600

    title svg

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,14 @@ export default {
         roboto: ['Roboto', 'sans-serif'],
       },
     },
+    colors:{
+      backgroundWhite: '#FFFFFF',
+      cardLight: '#F4E8E8',
+      cardDark: '#2D3C62',
+      textBlack: '#000000',
+      subheadingGray: '#4A4A4A',
+      subheadingLightGray: '#949090',
+    }
   },
   plugins: [],
 }
