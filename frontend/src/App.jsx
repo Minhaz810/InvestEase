fec2c35bde4bb0b2ee3ae6aef3ed7d34d32c3dee
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-09-13 21:01:27 +0600

    Sign In Component Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import SignUp from "./pages/Authentication/SignUp"
-
+import SignIn from "./pages/Authentication/SignIn"
 function App() {
 
 
   return (
     <>
-      <SignUp/>
+    <SignIn/>      
     </>
   )
 }
--- frontend/src/pages/Authentication/SignIn.jsx
+++ frontend/src/pages/Authentication/SignIn.jsx
@@ -2,10 +2,10 @@ import IntroCard from "../../components/IntroCard";
 import InputField from "../../components/Input";
 import PrimaryButton from "../../components/PrimaryButton";
 
-const SignUp = () =>{
+const SignIn = () =>{
     return(
         <>
-        <div className="flex justify-between gap-40">
+        <div className="flex justify-between gap-40 items-center">
             <div className="w-5/12">
             <IntroCard/>
             </div>
@@ -13,20 +13,11 @@ const SignUp = () =>{
             <div className="w-7/12 mt-20 pr-64">
                 <div>
                     <div className="text-textBlack text-primaryHeader font-roboto font-bold leading-tight">
-                        Get Started
+                        Sign In
                     </div>
                     <div className="text-subheadingLightGray text-subHeader font-roboto leading-tight mt-4">
-                        Create Your Acccount Now
+                        Sign In To Your Account
                     </div>
-                </div>
-
-                <div className="mt-16">
-                    <InputField
-                        type="text"
-                        placeholder="Enter Your Name"
-                        className="w-full p-3 border border-subheadingLightGray rounded-md focus:outline-none focus:border-primary"
-                        required={true}
-                    />
                 </div>
                 <div className="mt-8">
                     <InputField
@@ -44,30 +35,25 @@ const SignUp = () =>{
                         required={true}
                     />
                 </div>
-                <div className="mt-8">
-                    <InputField
-                        type="password"
-                        placeholder="Confirm Password"
-                        className="w-full p-3 border border-subheadingLightGray rounded-md focus:outline-none focus:border-primary"
-                        required={true}
-                    />
-                </div>
+
                 <div className="mt-12">
 
                     <PrimaryButton
                         className="w-full p-3 text-backgroundWhite"
-                        text = "Sign Up"
+                        text = "Sign In"
                     />
+                     <div className="text-right font-bold text-cardDark font-roboto mt-2">
+                        Forgot Password
+                    </div>
                 </div>
 
                 <div className="mt-8 text-center text-subheadingGray font-medium font-roboto ">
-                    Already Have An Account? <span className="text-cardDark font-bold">Sign In</span>
+                    Don't Have Any Account? <span className="text-cardDark font-bold">Registration</span>
                 </div>
             </div>
-
         </div>
         </>
     )
 }
 
-export default SignUp
+export default SignIn
