a62897ace668f261fbe6aadc3015a2e02dcea68f
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-09-16 15:15:45 +0600

    otp functionality added

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from users.views.user_registration import UserRegistration
+from users.views.user_verification import UserVerification
 
 urlpatterns = [
-    path('registration',view=UserRegistration.as_view(),name = 'user_registration')
+    path('registration',view=UserRegistration.as_view(),name = 'user_registration'),
+    path('verify-user',view = UserVerification.as_view(),name = 'user_verification' )
 ]
